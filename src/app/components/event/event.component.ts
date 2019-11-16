684617e1d8baad904b795610fd371cc3c141b5bf
Jhon Tejada <jhontejada13@gmail.com>
2019-11-15 23:37:55 -0500

    termmina la aplicacion hasta el punto pedido

---
+++ src/app/components/event/event.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input, EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-event',
+  templateUrl: './event.component.html',
+})
+
+export class EventComponent {
+  @Input() value: any;
+  @Output() deleteEventInstanceEvent: EventEmitter<any> = new EventEmitter<any>();
+  handleDeleteClick() {
+    this.deleteEventInstanceEvent.emit(this.value);
+  }
+}
